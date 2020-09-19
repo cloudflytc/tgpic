cdeabc635c36c1e3074fad158c7bd7a427fac383
cloudflytc <61569348+cloudflytc@users.noreply.github.com>
2020-09-19 16:43:33 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * FILE_NAME: index.php
+ * From: www.hidove.cn
+ * Author: Ivey
+ * Date: 2019/10/10 14:27
+ */
+?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <title>tg图床</title>
+    <?php include "static/view/head.html"; ?>
+    <script crossorigin="anonymous" integrity="sha384-4RTCcfPYk9Ynbsqfb8mxO6fADyCH+Hd9RaSvW6xO9/icE1biaZ1uw/qCx2BdQB2n"
+        src="https://lib.baomitu.com/bootstrap-fileinput/5.0.6/js/fileinput.min.js"></script>
+    <script crossorigin="anonymous" integrity="sha384-LBn99WQzkTDqv2h5L8yIOywHmqQvPsnkZZwxIg5ikQnHP2WbXhrKdLsUF65TgANs"
+        src="https://lib.baomitu.com/bootstrap-fileinput/5.0.6/js/plugins/piexif.min.js"></script>
+    <script crossorigin="anonymous" integrity="sha384-X03VuPfgj8rBD0thhiay5PNx+BvsaJZFXcfIsgh259SWkOoVuZdnw+OykP2UG7lJ"
+        src="https://lib.baomitu.com/bootstrap-fileinput/5.0.6/js/plugins/purify.min.js"></script>
+    <script crossorigin="anonymous" integrity="sha384-cuvJnJFRVnh5sfgGvFy3Yq55ljj4zer/VhFoCJp9puj5+3GR8s4cT48vqPafhLtu"
+        src="https://lib.baomitu.com/bootstrap-fileinput/5.0.6/themes/fas/theme.min.js"></script>
+</head>
+<body>
+    <?php include "static/view/nav.html"; ?>
+    <div class="container Hidove-main">
+        <div class="page-header">
+            <h1>图片上传</h1>
+            <p>最大5M，一次性最多10张。</p>
+        </div>
+       
+
+        <div class="post">
+            <form enctype="multipart/form-data">
+                <div class="form-group">
+                    <input id="Hidove" type="file" name="image" multiple="true" class="file"
+                        data-overwrite-initial="false" data-min-file-count="1" data-max-file-count="10"
+                        accept="image/*">
+                </div>
+            </form>
+        </div>
+        <div id="showurl" style="display:none;">
+            <ul id="navTab" class="nav nav-tabs">
+                <li class="nav-item">
+                    <a aria-selected="true" data-toggle="tab" role="tab" class="nav-link active" href="#urlcode">URL</a>
+                </li>
+                <li class="nav-item"><a aria-selected="false" data-toggle="tab" role="tab" class="nav-link"
+                        href="#htmlcode">HTML</a></li>
+                <li class="nav-item"><a aria-selected="false" data-toggle="tab" role="tab" class="nav-link"
+                        href="#bbcode">BBCode</a></li>
+                <li class="nav-item"><a aria-selected="false" data-toggle="tab" role="tab" class="nav-link"
+                        href="#markdown">Markdown</a></li>
+                <li class="nav-item"><a aria-selected="false" data-toggle="tab" role="tab" class="nav-link"
+                        href="#markdownwithlink">Markdown with Link</a></li>
+            </ul>
+            <div id="navTabContent" class="tab-content">
+                <div id="urlcode" class="tab-pane active show">
+                    <div class="card card-default">
+                        <div class="card-body Hidove-imageucode">
+                            <table class="table table-bordered">
+                                <tbody id="urlcodes">
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div id="htmlcode" class="tab-pane">
+                    <div class="card card-default">
+                        <div class="card-body Hidove-imageucode">
+                            <table class="table table-bordered">
+                                <tbody id="htmlcodes">
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div id="bbcode" class="tab-pane">
+                    <div class="card card-default">
+                        <div class="card-body Hidove-imageucode">
+                            <table class="table table-bordered">
+                                <tbody id="bbcodes">
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div id="markdown" class="tab-pane">
+                    <div class="card card-default">
+                        <div class="card-body Hidove-imageucode">
+                            <table class="table table-bordered">
+                                <tbody id="markdowncodes">
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div id="markdownwithlink" class="tab-pane">
+                    <div class="card card-default">
+                        <div class="card-body Hidove-imageucode">
+                            <table class="table table-bordered">
+                                <tbody id="markdowncodes2">
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php include "static/view/foot.html"; ?>
+<script src="static/js/upload.js"></script>
+</body>
+</html>
---
+++ upload.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * FILE_NAME: upload.php
+ * From: www.hidove.cn
+ * Author: Ivey
+ * Date: 2019/10/10 14:28
+ */
+
+if(empty($_FILES['image'])){
+    die(msg(400,'The image can not be null!'));
+}else{
+    $file = $_FILES['image'];
+}
+$imageInfo =getImageInfo($file['tmp_name']);
+if(!$imageInfo)
+    die(msg(400,'The image is illegal!'));
+$newFileName = $file['tmp_name'].'.'.$imageInfo['type'];
+rename($file['tmp_name'],$newFileName);
+$body["file"] = new \CURLFile(realpath($newFileName));
+//print_r($body);
+$response = HidoveCurlPost('https://telegra.ph/upload',$body);
+$response = json_decode($response,true);
+//echo($response[0]['src']);
+$imagesUrl['url'][0] = "https://i".rand(0,4).".wp.com/telegra.ph".$response[0]['src'];
+$imagesUrl['url'][1] = "https://telegra.ph".$response[0]['src'];
+$imagesUrl['url'][2] = "https://images.weserv.nl/?url=".$imagesUrl['url'][1];
+//print_r($$imagesUrl['url'][0]imagesUrl);
+die(msg(200,'success',$imagesUrl));
+function HidoveCurlPost($url,$post){
+    // 创建一个新 cURL 资源
+    $curl = curl_init();
+    // 设置URL和相应的选项
+    // 需要获取的 URL 地址
+    curl_setopt($curl, CURLOPT_URL,$url);
+    #启用时会将头文件的信息作为数据流输出。
+    curl_setopt($curl, CURLOPT_HEADER, false);
+    #在尝试连接时等待的秒数。设置为 0，则无限等待。
+    // curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
+    #允许 cURL 函数执行的最长秒数。
+    // curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+    #设置请求信息
+    //设置post方式提交
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl,CURLOPT_POSTFIELDS,$post);
+    #关闭ssl
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+    #TRUE 将 curl_exec获取的信息以字符串返回，而不是直接输出。
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    // 抓取 URL 并把它传递给浏览器
+    $return = curl_exec($curl);
+    if ($return===false) {
+        return "CURL Error:".curl_error($curl);
+    }
+    curl_close($curl);
+    return $return;
+}
+function getImageInfo($fileName){
+    $data = getimagesize($fileName);
+    switch ($data[2]){
+        case 1:return [
+            'type'=>'gif',
+            'mime'=> $data['mime']
+        ];
+        case 2:return [
+            'type'=>'jpg',
+            'mime'=> $data['mime']
+        ];
+        case 3:
+            return [
+                'type'=>'png',
+                'mime'=> $data['mime']
+            ];
+        case 6:
+            return [
+                'type'=>'bmp',
+                'mime'=> $data['mime']
+            ];
+        case 17:
+            return [
+                'type'=>'ico',
+                'mime'=> $data['mime']
+            ];
+        default:
+            return false;
+    }
+}
+function msg($code,$msg,$data=[]){
+    return json_encode([
+        'code'=>$code,
+        'msg'=>$msg,
+        'data'=>$data,
+    ]);
+}
